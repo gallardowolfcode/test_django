6187ba9313aee36fdd6dee519a33d48bc7bdefd5
FERNANDO <fearnandogallardomartinez@gmail.com>
2021-03-28 14:02:54 -0600

    corrección UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -6,4 +6,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'first_name']
+        fields = ['id', 'email', 'first_name']
