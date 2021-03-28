02a0ecbd8fbf09c0a9e5e75c1515457efeee5676
FERNANDO <fearnandogallardomartinez@gmail.com>
2021-03-28 13:54:13 -0600

    url

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Employee, User, Request
+from .serializers import EmployeeSerializer, UserSerializer, RequestSerializer
 
 # Create your views here.
+class UserViewset(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class EmployeeViewset(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class RequestViewset(viewsets.ModelViewSet):
+    queryset = Request.objects.all()
+    serializer_class = RequestSerializer
